04dcdf7154c865be4ea0977c06ee95c1f906e28d
MasterOfSkillIssue <skillissuecmaster@gmail.com>
2024-12-29 17:59:41 +0200

    filtered warnings in GUI

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import skimage
+from skimage.color import rgb2lab, lab2rgb
+from skimage.transform import resize
 print("working")
+print(st.__version__,
+plt.__version__,
+np.__version__,
+torch.__version__,
+skimage.__version__)
